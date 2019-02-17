ec154c7497f3e4c3dfcbded7b1692e8dca1be7ef
Raffael Wyss <raffael.wyss@gmail.com>
2019-02-17 21:02:57 +0100

    Dynamischer Aufbau implementiert (Config/Version)

--- src/App.php
+++ src/App.php
@@ -12,11 +12,21 @@ class App
 		$server = new Server();
 		if ($server->getParameter('create') === null) {
 			$html = file_get_contents('../templates/markup.html');
+			$html = $this->templateReplaceData($html);
 			echo $html;
 		} else {
 			$maker = new Maker();
 			$maker->make($server);
 		}
+	}
+
+	private function templateReplaceData($html)
+	{
+		$hostConfig = json_decode(file_get_contents('../config/host.local'), true);
+		$version = file_get_contents('../version');
+		$html = str_replace('{basePath}', $hostConfig['basePath'], $html);
+		$html = str_replace('{version}', $version, $html);
+		return $html;
 	}
 }
 

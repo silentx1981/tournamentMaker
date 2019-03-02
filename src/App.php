750b85b633560dc20e91a9a681e33e47f72460eb
Raffael Wyss <raffael.wyss@gmail.com>
2019-03-02 08:37:32 +0100

    Change Markup-Teamplate and css-styles

--- src/App.php
+++ src/App.php
@@ -23,9 +23,13 @@ class App
 	private function templateReplaceData($html)
 	{
 		$hostConfig = json_decode(file_get_contents('../config/host.local'), true);
+		$hostConfig['basePath'] = $hostConfig['basePath'] ?? '/';
+		$hostConfig['name'] = $hostConfig['name'] ?? 'TournamentMaker';
 		$version = file_get_contents('../version');
 		$html = str_replace('{basePath}', $hostConfig['basePath'], $html);
+		$html = str_replace('{name}', $hostConfig['name'], $html);
 		$html = str_replace('{version}', $version, $html);
+		$html = str_replace('{imageLogo}', $hostConfig['images']['logo'] ?? null, $html);
 		return $html;
 	}
 }
